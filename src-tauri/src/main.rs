278bd0a13b687ec9baa591553a629582bd59aa1d
Ryan Scott Brown <sb@ryansb.com>
2023-10-21 11:05:37 -0400

    initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn check_session_valid() -> bool {
+    println!("check_session_valid");
+    true
+}
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // for debug builds, open the devtools by default
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
