6fbbfb09bb7eaf9dfab342cdfdf28f6f92e6bd89
Ryan Scott Brown <sb@ryansb.com>
2023-11-19 14:57:43 -0500

    Change to teal UI theme

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,26 @@ import { md3 } from 'vuetify/blueprints'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
+// 2023-11-19 no types are available for colors in vuetify 3
+// @ts-expect-error-next-line
+import colors from 'vuetify/lib/util/colors'
+
 
 
 const vuetify = createVuetify({
     components,
     blueprint: md3,
+    theme: {
+        themes: {
+            light: {
+                dark: false,
+                colors: {
+                    primary: colors.teal.darken1,
+                    secondary: colors.teal.lighten4,
+                }
+            }
+        }
+    },
     icons: {
         defaultSet: 'mdi',
         aliases,
