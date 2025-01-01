f685fdbc8d1a92a2f1d6bcc0c3a87e06fabd63c8
Ryan Scott Brown <sb@ryansb.com>
2025-01-01 18:53:40 -0500

    feat: capture stray keypresses and make them search terms

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, onMounted, onUnmounted } from "vue";
 import { useSessionStore } from "../store";
 import { nextTick } from "vue";
 
@@ -10,6 +10,38 @@ const store = useSessionStore();
 
 const DEBOUNCE_MS = 250;
 let timeout: number;
+
+
+const handleKeydown = (event: KeyboardEvent) => {
+    // on `Esc`, clear and hide the search box
+    if (event.key === 'Escape') {
+        search.value = false;
+        searchTerms.value = '';
+        return
+    }
+
+    if (
+        event.target instanceof HTMLInputElement ||
+        event.target instanceof HTMLTextAreaElement
+    ) return
+
+    // for regular characters, show and focus the search box
+    // VTextField will capture the key press.
+    if (event.key.length === 1) {
+        search.value = true;
+        nextTick(() => {
+            searchBox.value?.focus();
+        });
+    }
+};
+
+onMounted(() => {
+    window.addEventListener('keydown', handleKeydown);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeydown);
+});
 
 watch(searchTerms, (terms) => {
     timeout && clearTimeout(timeout);
@@ -33,6 +65,6 @@ watch(search, (isShown) => {
     <VAppBarNavIcon @click="search = !search; searchTerms = ''">
         <VIcon>mdi-magnify</VIcon>
     </VAppBarNavIcon>
-    <VTextField v-if="search" ref="searchBox" v-model="searchTerms" hide-details placeholder="Search" single-line clearable
-        color="secondary" persistent-clear />
+    <VTextField v-if="search" ref="searchBox" v-model="searchTerms" hide-details placeholder="Search" single-line
+        clearable color="secondary" persistent-clear />
 </template>
